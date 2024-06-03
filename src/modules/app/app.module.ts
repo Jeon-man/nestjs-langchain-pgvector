a952b03014068870e8fa3f5e671d9434043210b0
Jeon-man <jhmjhm0106@naver.com>
2024-06-03 14:48:24 +0900

    FEAT: Embed pdf

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configDynamicModule } from '@module/config/config.module';
 import { LangChainModule } from '@module/langchain/langchain.module';
+import { VectorStoreModule } from '@module/vectorStore/vectorStore.module';
 
 @Module({
-  imports: [configDynamicModule, LangChainModule],
+  imports: [configDynamicModule, LangChainModule, VectorStoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/langchain/langchain.service.ts
+++ src/modules/langchain/langchain.service.ts
@@ -1,4 +1,14 @@
+import { VectorStoreProvider } from '@module/vectorStore/vectorStore.service';
 import { Injectable } from '@nestjs/common';
+import path from 'path';
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
 
 @Injectable()
-export class LangChainService {}
+export class LangChainService {
+  constructor(private readonly vectorStore: VectorStoreProvider) {}
+
+  async embeddingPdf() {
+    const pdfLoader = new PDFLoader('src/');
+    const pdf = pdfLoader.load();
+  }
+}
