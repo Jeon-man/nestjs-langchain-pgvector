cc400d4a994c97127f67bae487a3f7fda388e333
Jeon-man <gusals236@gmail.com>
2024-06-03 15:23:09 +0900

    FIX: Wrong module

--- src/modules/langchain/langchain.module.ts
+++ src/modules/langchain/langchain.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LangChainController } from './langchain.controller';
 import { VectorStoreModule } from '@module/vectorStore/vectorStore.module';
+import { LangChainService } from './langchain.service';
 
 @Module({
-  providers: [VectorStoreModule],
+  imports: [VectorStoreModule],
+  providers: [LangChainService],
   controllers: [LangChainController],
 })
 export class LangChainModule {}
