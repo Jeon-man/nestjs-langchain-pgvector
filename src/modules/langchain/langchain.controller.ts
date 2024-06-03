f2ab0ba3aacccc0bd2195ee1bf7966ae7a6d6a6e
Jeon-man <gusals236@gmail.com>
2024-06-03 16:16:39 +0900

    FEAT: Add test file embedding

--- src/modules/langchain/langchain.controller.ts
+++ src/modules/langchain/langchain.controller.ts
@@ -1,9 +1,18 @@
 import { Controller, Post } from '@nestjs/common';
+import { LangChainService } from './langchain.service';
 
 @Controller('lang-chain')
 export class LangChainController {
-  constructor() {}
+  constructor(private readonly langchainService: LangChainService) {}
 
   @Post('pdf')
-  async readPdf() {}
+  async readPdf() {
+    try {
+      await this.langchainService.embeddingPdf();
+    } catch (err) {
+      throw err;
+    }
+
+    return true;
+  }
 }
--- src/modules/langchain/langchain.service.ts
+++ src/modules/langchain/langchain.service.ts
@@ -1,14 +1,38 @@
 import { VectorStoreProvider } from '@module/vectorStore/vectorStore.service';
 import { Injectable } from '@nestjs/common';
-import path from 'path';
+import { Document } from '@langchain/core/documents';
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import appRootPath from 'app-root-path';
 
 @Injectable()
 export class LangChainService {
   constructor(private readonly vectorStore: VectorStoreProvider) {}
 
   async embeddingPdf() {
-    const pdfLoader = new PDFLoader('src/');
-    const pdf = pdfLoader.load();
+    const pdfLoader = new PDFLoader(appRootPath + '/storage/test.pdf');
+    const pdf = await pdfLoader.load();
+
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 50,
+    });
+
+    const texts = await textSplitter.splitDocuments(pdf);
+    let embeddings: Document[] = [];
+
+    for (let i = 0; i < texts.length; i++) {
+      const page = texts[i];
+      const splitTexts = await textSplitter.splitText(page.pageContent);
+
+      const pageEmbeddings = splitTexts.map(text => ({
+        pageContent: text,
+        metadata: {
+          pageNumber: i,
+        },
+      }));
+      embeddings = embeddings.concat(pageEmbeddings);
+    }
+    await this.vectorStore.addDocument(embeddings);
   }
 }
