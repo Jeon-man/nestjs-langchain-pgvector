976451333a24f66205c092282cddd15f722e5382
Jeon-man <gusals236@gmail.com>
2024-06-04 16:23:49 +0900

    REFACTOR: Use korean

--- src/utils/makeChain.ts
+++ src/utils/makeChain.ts
@@ -5,6 +5,7 @@ import { StringOutputParser } from '@langchain/core/output_parsers';
 import type { Document } from '@langchain/core/documents';
 import type { VectorStoreRetriever } from '@langchain/core/vectorstores';
 
+// To use the answer in another language, change the bottom sentence.
 const CONDENSE_TEMPLATE = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
 <chat_history>
@@ -12,11 +13,14 @@ const CONDENSE_TEMPLATE = `Given the following conversation and a follow up ques
 </chat_history>
 
 Follow Up Input: {question}
-Standalone question:`;
+Standalone question.
+Since the user who sees the answer is Korean, the answer MUST always be Korean.`;
 
+// To use the answer in another language, change the bottom sentence.
 const QA_TEMPLATE = `You are an expert researcher. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context or chat history, politely respond that you are tuned to only answer questions that are related to the context.
+Since the user who sees the answer is Korean, the answer MUST always be Korean.
 
 <context>
   {context}
