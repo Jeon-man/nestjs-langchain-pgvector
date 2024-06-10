7fc1085cb4dfa02eaab003b439fa7e05ce33357e
Jeon-man <jhmjhm0106@naver.com>
2024-06-10 14:57:54 +0900

    FIX: Delete unused module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configDynamicModule } from '@module/config/config.module';
 import { LangChainModule } from '@module/langchain/langchain.module';
-import { VectorStoreModule } from '@module/vectorStore/vectorStore.module';
 import { CustomWinstonModule } from '@module/winston/winston.module';
 import { LoggerMiddleware } from '@module/logger/logger.middleware';
 
 @Module({
-  imports: [configDynamicModule, LangChainModule, VectorStoreModule, CustomWinstonModule],
+  imports: [configDynamicModule, LangChainModule, CustomWinstonModule],
   controllers: [AppController],
   providers: [AppService],
 })
