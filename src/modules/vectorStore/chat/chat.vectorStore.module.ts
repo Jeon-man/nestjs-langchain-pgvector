59da5ee59f283e43d2f2742c4b78b53c2d504ec9
Jeon-man <jhmjhm0106@naver.com>
2024-06-11 15:16:05 +0900

    FIX: Wrong module

--- src/modules/vectorStore/chat/chat.vectorStore.module.ts
+++ src/modules/vectorStore/chat/chat.vectorStore.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ChatVectorStoreStrategy } from './chat.vectorStore.strategy';
 
 @Module({
-  providers: [ChatVectorStoreModule],
-  exports: [ChatVectorStoreModule],
+  providers: [ChatVectorStoreStrategy],
+  exports: [ChatVectorStoreStrategy],
 })
 export class ChatVectorStoreModule {}
